a95889618f8538fc724e2ac70ea34675797744ba
Neil Dwyer <neildwyer1991@gmail.com>
2021-05-24 14:56:46 -0700

    building aar with webrtc replaced with unityrtc

--- build_android/1-fetch.sh
+++ build_android/1-fetch.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+WK_DIR=$(pwd)
+
 source ./variables.sh
+
+cd /tmp
+rm -rf src
 fetch --nohooks webrtc_android
 cd src
 git checkout "refs/remotes/branch-heads/$WEBRTC_VERSION"
+cd ..
+mv src "$WK_DIR"
+cd "$WK_DIR"
 gclient sync -f
--- build_android/2-patch.sh
+++ build_android/2-patch.sh
@@ -1,23 +1,45 @@
 set -e
 source variables.sh
 
+SRC_DIR=$(pwd)/src
+SDK_DIR=$(pwd)/src/sdk/android
+RTC_BASE_DIR=$(pwd)/src/rtc_base
+MODULES_DIR=$(pwd)/src/modules
+JNI_DIR=$(pwd)/src/sdk/android/src/jni
+
 cd src
 
 git stash
-git checkout "refs/remotes/branch-heads/$WEBRTC_VERSION"
-set +e
-git branch -D rename
-set -e
+git clean -fd
 
 cd ..
 
-git checkout -b rename
-
 patch -N "src/BUILD.gn" < "patches/add_jsoncpp.patch"
 
-cd src
+cd "$SDK_DIR"
 
+sed -i 's/api\/org\/webrtc/api\/org\/unityrtc/g' BUILD.gn
+sed -i 's/java\/org\/webrtc/java\/org\/unityrtc/g' BUILD.gn
+mv "$SDK_DIR/api/org/webrtc" "$SDK_DIR/api/org/unityrtc"
+mv "$SDK_DIR/src/java/org/webrtc" "$SDK_DIR/src/java/org/unityrtc"
+find . -type f -name "*.java" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
+find . -type f -name "*.java" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
 
-sed -i 's/package\ org.webrtc/package\ org.unityrtc/g' *.java
-sed -i 's/package\ org.webrtc/package\ org.unityrtc/g' audio/*.java
+cd "$RTC_BASE_DIR"
+mv "$RTC_BASE_DIR/java/src/org/webrtc" "$RTC_BASE_DIR/java/src/org/unityrtc"
+sed -i 's/java\/src\/org\/webrtc/java\/src\/org\/unityrtc/g' BUILD.gn
+find . -type f -name "*.java" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
+find . -type f -name "*.java" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
 
+# /src/modules/audio_device/android/java/src/org/webrtc/voiceengine/WebRtcAudioTrack
+cd "$MODULES_DIR"
+mv "$MODULES_DIR/audio_device/android/java/src/org/webrtc" "$MODULES_DIR/audio_device/android/java/src/org/unityrtc"
+sed -i 's/java\/src\/org\/webrtc/java\/src\/org\/unityrtc/g' audio_device/BUILD.gn
+find . -type f -name "*.java" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
+find . -type f -name "*.java" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
+
+cd "$SRC_DIR"
+find . -type f -name "*.cc" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
+find . -type f -name "*.h" -exec sed -i 's/org\.webrtc/org\.unityrtc/g' {} +
+find . -type f -name "*.cc" -exec sed -i 's/org_webrtc/org_unityrtc/g' {} +
+find . -type f -name "*.h" -exec sed -i 's/org_webrtc/org_unityrtc/g' {} +
