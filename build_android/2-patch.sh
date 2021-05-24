67fa7f70f39f43575da7e75bfb5694a400fafc44
Neil Dwyer <neildwyer1991@gmail.com>
2021-05-24 09:49:02 -0700

    working on the rename script now

--- build_android/2-patch.sh
+++ build_android/2-patch.sh
@@ -1,2 +1,23 @@
+set -e
+source variables.sh
+
+cd src
+
+git stash
+git checkout "refs/remotes/branch-heads/$WEBRTC_VERSION"
+set +e
+git branch -D rename
+set -e
+
+cd ..
+
+git checkout -b rename
+
 patch -N "src/BUILD.gn" < "patches/add_jsoncpp.patch"
+
+cd src
+
+
+sed -i 's/package\ org.webrtc/package\ org.unityrtc/g' *.java
+sed -i 's/package\ org.webrtc/package\ org.unityrtc/g' audio/*.java
 
