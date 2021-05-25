d12b1a5e582e57a4d65f13ee7333a3b6bbf5ae4b
Neil Dwyer <neildwyer1991@gmail.com>
2021-05-25 15:57:54 -0700

    Small updates

--- build_plugin_android/3-rename.sh
+++ build_plugin_android/3-rename.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+SRC_DIR=$(pwd)/src
+
 cd src/Plugin~/tools
 
+# Plugin~/WebRTCPlugin/Codec/AndroidCodec/android_codec_factory_helper.cc
+
 find . -type f -name "*.txt" -exec sed -i 's/Java_org_webrtc/Java_org_unityrtc/g' {} +
+
+cd "$SRC_DIR/Plugin~/WebRTCPlugin/Codec"
+
+find . -type f -name "*.cc" -exec sed -i 's/org\/webrtc/org\/unityrtc/g' {} +
+find . -type f -name "*.h" -exec sed -i 's/org\/webrtc/org\/unityrtc/g' {} +
+
--- build_plugin_android/5-release.sh
+++ build_plugin_android/5-release.sh
@@ -2,7 +2,7 @@
 set -e
 
 RELEASE_DIR=/tmp/webrtcrelease
-REPO="git@github.com:chef-studios-inc/unitywebrtc.git"
+REPO="git@github.com:chef-studios-inc/com.unity.webrtc.git"
 BUILD_DIR="$(pwd)/src"
 
 set +e
@@ -15,13 +15,18 @@ cd "$RELEASE_DIR"
 
 git clone "$REPO"
 
-cd unitywebrtc
+cd com.unity.webrtc 
 
 cp -r "$BUILD_DIR/Runtime" .
 cp -r "$BUILD_DIR/Editor" .
+cp -r "$BUILD_DIR/Samples~" .
 cp "$BUILD_DIR/package.json" .
 cp "$BUILD_DIR/package.json.meta" .
 cp "$BUILD_DIR/.gitignore" .
+cp "$BUILD_DIR/Editor.meta" .
+cp "$BUILD_DIR/Runtime.meta" .
+rm -rf "Runtime/Plugins/iOS"
+rm "Runtime/Plugins/iOS.meta"
 
 git add .
 git commit -m "Release"
